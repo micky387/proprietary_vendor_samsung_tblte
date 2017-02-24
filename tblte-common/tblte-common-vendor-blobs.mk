e67bf32b4aa528b34e5bd939dba743e483d3b51f
underscoremone <underscoremone@gmail.com>
2017-02-24 13:56:17 +0000

    tblte: add aptX codec support
    Change-Id: Ia9cf6f648953be5a225ee4f16dc870f30f535f98

--- tblte-common/tblte-common-vendor-blobs.mk
+++ tblte-common/tblte-common-vendor-blobs.mk
@@ -16,6 +16,9 @@
 
 PRODUCT_COPY_FILES += \
     vendor/samsung/tblte-common/proprietary/bin/adsprpcd:system/bin/adsprpcd \
+    vendor/samsung/tblte-common/proprietary/vendor/lib/libaptX-1.0.0-rel-Android21-ARMv7A.so:system/vendor/lib/libaptX-1.0.0-rel-Android21-ARMv7A.so \
+    vendor/samsung/tblte-common/proprietary/vendor/lib/libaptXHD-1.0.0-rel-Android21-ARMv7A.so:system/vendor/lib/libaptXHD-1.0.0-rel-Android21-ARMv7A.so \
+    vendor/samsung/tblte-common/proprietary/vendor/lib/libaptXScheduler.so:system/vendor/lib/libaptXScheduler.so \
     vendor/samsung/tblte-common/proprietary/vendor/lib/libadsprpc.so:system/vendor/lib/libadsprpc.so \
     vendor/samsung/tblte-common/proprietary/vendor/lib/libfastcvadsp_stub.so:system/vendor/lib/libfastcvadsp_stub.so \
     vendor/samsung/tblte-common/proprietary/vendor/lib/libfastcvopt.so:system/vendor/lib/libfastcvopt.so \
